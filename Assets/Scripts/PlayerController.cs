ba8815258ab8a5a7cee9aacedf47a61bcf2fbf8c
mebekah <bekahrussell@gmail.com>
2020-08-24 12:39:44 -0400

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Resources;
+using UnityEngine;
+
+public class PlayerController: MonoBehaviour
+{
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+    
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+       // moveSpeed
+    }
+}
